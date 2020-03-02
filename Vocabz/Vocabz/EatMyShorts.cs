b91937384545b1b53c3c9f2f69b6c9514fed689c
Matutin <tom.kasper@posteo.de>
2020-03-02 12:24:34 +0100

    EatMyShorts() added

---
+++ Vocabz/Vocabz/EatMyShorts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocabz
+{
+    class EatMyShorts
+    {
+        public void EatMyShorts()
+        {
+            for (int i = 0; i < shorts.length; i++)
+            {
+                Eat();
+            }
+        }
+    }
+}
