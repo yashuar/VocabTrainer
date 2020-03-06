32ae3f22e07997deedfe0ed2e1312065c807d834
Andre <pfox803@gmail.com>
2020-03-06 08:44:38 +0100

    new Function RadomizeList new Function GetVokabelList new Function Shuffle

--- Vocabz/Vocabz/Presenter.cs
+++ Vocabz/Vocabz/Presenter.cs
@@ -8,5 +8,35 @@ namespace Vocabz
 {
     class Presenter
     {
+
+        public List<T> RandomizeList<T>()
+        {
+            List<T> originalList = GetVokabelList();
+            List<T> randomizedList = Shuffle(originalList);
+            return randomizedList;
+        }
+
+        public List<T> GetVokabelList<T>()
+        {
+            List<T> vocabList = XMLSerializer.getList();
+            return vocabList;
+        }
+
+        private static Random rng = new Random();
+
+        public static List<T> Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
+        }
+
     }
 }
